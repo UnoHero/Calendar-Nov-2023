23ab4ab88c409880314b967c4fb0b9266df083ee
Scott <scottunogundersen@gmail.com>
2023-11-21 15:16:49 +0100

    created working arrow movements.

--- app.js
+++ app.js
@@ -23,14 +23,24 @@ app.get('/', (req, res) => {
 app.get("/november", (req, res) => {
   res.render("index");
 })
-  
+
+app.get("/november/0", (req, res) => {
+  res.redirect('/november');
+})
+
+app.get("/november/NaN", (req, res) => {
+  res.redirect('/november/1');
+})
+
 app.get(`/november/:day`, (req, res) => {
   const day = parseInt(req.params.day);
   console.log(day);
-  res.sendFile("content.html", {root: `2023/November/${day}.` })
+  res.sendFile("content.html", {root: `public/2023/November/${day}.` })
+
 })
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render("404", {title: "404"});
 });
+
